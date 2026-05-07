8eedd270cecba9922efa492bb10da1f5f8785093
BawiPOANIMA <christophepoanima@gmail.com>
2026-05-07 14:33:52 +1100

    Update and rename test to data.php

---
+++ TP1/data.php
@@ -0,0 +1,13 @@
+<body>
+
+    <PARTIE MESURE A LAISSER>
+
+    <h1>Commande ESP32</h1>
+
+    <button onclick="fetch('http://IP_ESP32/son')">
+        🔆 Allumer la Buzzer
+    </button>
+    <button onclick="fetch('http://IP_ESP32/led')">
+        🔆 Allumer la LED
+    </button>
+</body>
